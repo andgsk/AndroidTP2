d6f0356101351b73e7ea9483aa08d82a985d9705
ObeliskDev <crashbaco@gmail.com>
2016-02-23 13:32:25 -0500

    added fragment login and inscription in activity + navigation de base

--- app/src/main/java/deptinfo/cegepgarneau/ca/tp2/fragments/InscriptionFragment.java
+++ app/src/main/java/deptinfo/cegepgarneau/ca/tp2/fragments/InscriptionFragment.java
@@ -1,11 +1,13 @@
 package deptinfo.cegepgarneau.ca.tp2.fragments;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import deptinfo.cegepgarneau.ca.tp2.R;
 
 /**
  * Created by Renaud-Charles on 22/02/2016.
@@ -20,7 +22,8 @@ public class InscriptionFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_inscription,container,false);
+        return  view;
     }
 
 }
