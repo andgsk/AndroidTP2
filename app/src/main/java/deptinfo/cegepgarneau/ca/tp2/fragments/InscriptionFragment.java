ab515091c4f1064e4255e548e59af04461bf19c4
ObeliskDev <crashbaco@gmail.com>
2016-02-23 12:29:14 -0500

    removed actionbar for drawer. :3

---
+++ app/src/main/java/deptinfo/cegepgarneau/ca/tp2/fragments/InscriptionFragment.java
@@ -0,0 +1,26 @@
+package deptinfo.cegepgarneau.ca.tp2.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Renaud-Charles on 22/02/2016.
+ */
+public class InscriptionFragment extends Fragment {
+
+    // Constructeur
+    public InscriptionFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
