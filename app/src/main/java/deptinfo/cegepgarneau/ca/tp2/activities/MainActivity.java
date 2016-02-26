fac8188a84422b501e61ed6e12c4a303665704c7
andgsk <andgsk@gmail.com>
2016-02-26 13:46:57 -0500

    tabs pour le classement ^^

--- app/src/main/java/deptinfo/cegepgarneau/ca/tp2/activities/MainActivity.java
+++ app/src/main/java/deptinfo/cegepgarneau/ca/tp2/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package deptinfo.cegepgarneau.ca.tp2.activities;
 
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 import deptinfo.cegepgarneau.ca.tp2.R;
+import deptinfo.cegepgarneau.ca.tp2.adapters.TabsPagerAdapter;
+import deptinfo.cegepgarneau.ca.tp2.fragments.classBlockFragment;
+import deptinfo.cegepgarneau.ca.tp2.fragments.classVoieFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     public android.support.v4.app.FragmentTransaction fragmentTransaction;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,34 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout,
         mToolbar, R.string.txt_nav_header, R.string.txt_nav_header);
 
+
+        //Initialisation du viewPager et configuration du ViewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        //Initialisation du TabLayout
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        //Association du TabLayout au ViewPager.
+        tabLayout.setupWithViewPager(viewPager);
+
+        //Gestionnaire d'événements sur la sélection d'un onglet.
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(getApplicationContext(),"Selection de " + tab.getText(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                Toast.makeText(getApplicationContext(),"Désélection de " + tab.getText(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Toast.makeText(getApplicationContext(),"Resélection de " + tab.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+
         mDrawerLayout.setDrawerListener(toggle);
 
         toggle.syncState();
@@ -40,6 +75,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
         mNavigationView.setNavigationItemSelectedListener(this);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        //instanciation du FragmentPageAdapter pour la gestion
+        //des fragments lors du changement d'onglet.
+        TabsPagerAdapter adapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        //On obtient le titre des onglets à parir du string-array
+        //dans le fichier res/values/strings.xml
+        String[] tabs = getResources().getStringArray(R.array.tabs);
+
+        //Ajout des fragments
+        adapter.addFragment(new classBlockFragment(), tabs[0]);
+        adapter.addFragment(new classVoieFragment(), tabs[1]);
+        viewPager.setAdapter(adapter);
+    }
+
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
