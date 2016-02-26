b53d347ce39b43a81c7b890a0ecf429ba1c5263b
andgsk <andgsk@gmail.com>
2016-02-26 10:49:10 -0500

    ajout land

---
+++ app/src/main/java/deptinfo/cegepgarneau/ca/tp2/fragments/pistesFragmentListe.java
@@ -0,0 +1,24 @@
+package deptinfo.cegepgarneau.ca.tp2.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import deptinfo.cegepgarneau.ca.tp2.R;
+
+/**
+ * Created by Andrey on 2016-02-26.
+ */
+public class pistesFragmentListe extends Fragment {
+    public pistesFragmentListe() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_pistes_land,container,false);
+        return  view;
+    }
+}
---
+++ app/src/main/java/deptinfo/cegepgarneau/ca/tp2/fragments/pistesFragmentPiste.java
@@ -0,0 +1,7 @@
+package deptinfo.cegepgarneau.ca.tp2.fragments;
+
+/**
+ * Created by Andrey on 2016-02-26.
+ */
+public class pistesFragmentPiste {
+}
